14562f55ec366a7c6e2112017f7529c47419ebe2
ChenXin <chenxin.az@outlook.com>
2022-01-17 09:29:18 +0800

    fix autoupdater script

--- autoupdater/clash_updater.sh
+++ autoupdater/clash_updater.sh
@@ -4,18 +4,25 @@ WORK_DIR=/home/xin/projects/clash_on_n1
 UPDATER_VOLUME=${WORK_DIR}/volume/updater
 cd ${WORK_DIR}
 mkdir -p ${UPDATER_VOLUME}/{data,bak}
+cp autoupdater/*.* ${UPDATER_VOLUME}/
+
+# TODO: update config to match your provider's clash download link
+# the links should keep secret and DO NOT upload to git repo
+# cp autoupdater/conf/*.* ${UPDATER_VOLUME}/
 rm ${UPDATER_VOLUME}/data/updater.log
+
 docker run --rm -v ${UPDATER_VOLUME}:/autoupdater chenxinaz/clash_updater > ${UPDATER_VOLUME}/data/updater.log
 cat ${UPDATER_VOLUME}/data/updater.log
 cat ${UPDATER_VOLUME}/data/updater.log >> ${UPDATER_VOLUME}/data/updater_all.log
-cat "==================================" >> ${UPDATER_VOLUME}/data/updater_all.log
-if grep -Fxq "FAIL::" ${UPDATER_VOLUME}/data/updater.log; then
+echo "==================================" >> ${UPDATER_VOLUME}/data/updater_all.log
+
+if grep -Fq "FAIL::" ${UPDATER_VOLUME}/data/updater.log; then
     echo "Some provider config download fail, please check."
 else
     DT=$(date +%Y%m%d-%H%M%S)
     sed "s/🔰 节点选择/🔰 节点选择${DT}/g" ${UPDATER_VOLUME}/data/config.yaml > ${UPDATER_VOLUME}/data/config1.yaml
     cp ${UPDATER_VOLUME}/data/config1.yaml ${UPDATER_VOLUME}/bak/config.${DT}.yaml
     cp ${UPDATER_VOLUME}/data/config1.yaml ${WORK_DIR}/volume/clash/config.yaml
-    docker-compose restart
+    # docker-compose restart
 fi
 
--- tmp.py
+++
@@ -1,2 +0,0 @@
-from datetime import datetime, date
-print(datetime.now().strftime('%Y%m%d-%H%M%S'))
